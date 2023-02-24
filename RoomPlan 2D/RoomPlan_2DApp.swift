b49ac3fe8db31ba9b06e229894a842ce596ec401
Dennis van Oosten <denniswave95@gmail.com>
2023-02-24 12:52:40 +0100

    Initial Commit

---
+++ RoomPlan 2D/RoomPlan_2DApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomPlan_2DApp.swift
+//  RoomPlan 2D
+//
+//  Created by Dennis van Oosten on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RoomPlan_2DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
